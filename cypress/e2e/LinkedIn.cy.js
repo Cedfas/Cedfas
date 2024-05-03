10b0d34fed2978f857edee1f04bdd9b443735923
Cedfas <168768391+Cedfas@users.noreply.github.com>
2024-05-03 09:29:55 -0400

    Adding updates to my files
    Adding a little bit of logging for the LinkedIn Page when it returns a 999 Error from running the script too much.
    
    Signed-off-by: Cedfas <168768391+Cedfas@users.noreply.github.com>

--- cypress/e2e/LinkedIn.cy.js
+++ cypress/e2e/LinkedIn.cy.js
@@ -1,23 +1,35 @@
-import LinkedInFixtures from '../fixtures'
+import LinkedInFixtures from '../fixtures/linkedin_fixtures.cy'
 
 const LinkedIn = new LinkedInFixtures()
 
 describe('Opening LinkedIn Profile', () => {
   it('Finding Profile Via Google', () => {
     cy.visit('https://www.google.com')
-    LinkedInFixtures.clickGoogleField()
+    LinkedIn.clickGoogleField()
     cy.get('#jZ2SBf > .wM6W7d > span').click()
     cy.contains('Glenn Soles Jr').click()
     cy.origin('https://www.linkedin.com', () => {
       cy.get('#public_profile_contextual-sign-in > div > section > button > icon > svg').click()
     })
+    LinkedIn.JrSolesName().should('be.visible')
   })
 
-  it('Opening LinkedIn Profile Directly', () => {
-    cy.visit('https://www.linkedin.com/in/jrsoles/')
-    cy.origin('https://www.linkedin.com', () => {
-        cy.get('#public_profile_contextual-sign-in > div > section > button > icon > svg').click()
+  it('Checking Response Code for LinkedIn Profile', () => {
+    cy.request({
+      url:'https://www.linkedin.com/in/jrsoles/',
+      failOnStatusCode: false,
+    }).then((res) => {
+      if (res.status == 999) {
+        cy.log(JSON.stringify(res.body)) //prints the response body
+        cy.log(res.body.name) //prints name
+        cy.log(res.body.message) //prints message
+      } 
+      if (res.status == 200) {
+        cy.log(JSON.stringify(res.body)) //prints the response body
+        cy.log(res.body.name) //prints name
+        cy.log(res.body.message) //prints message
+      }
     })
   })
-   
+
 })
